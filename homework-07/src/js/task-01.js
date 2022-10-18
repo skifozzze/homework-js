e3f7df20b2eccc176a9da7d4fbc40a369febfc65
skifozzze <skifozzze@gmail.com>
2022-10-18 16:48:57 +0300

    1

---
+++ homework-07/src/js/task-01.js
@@ -0,0 +1,8 @@
+
+
+let numberOfCategories = document.querySelectorAll('.item').length;
+console.log(`у списку ${numberOfCategories} категорій`);
+
+
+const categoreisEl = document.querySelectorAll('h2');
+console.log(`${categoreisEl}`)
